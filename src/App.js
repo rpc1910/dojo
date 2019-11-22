e07cc736662afaf6c77d39a56f3c2d056b993387
Rodrigo Passos <rpassos.ext@vr.com.br>
2019-11-22 09:08:14 -0300

    Atualizado app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import GlobalStyle from './styles';
+import React from "react";
+import GlobalStyle from "./styles";
 
 export default class App extends React.Component {
-
   render() {
     return (
       <>
-        <h1>Olá</h1>
+        <h1>Olá Mundo</h1>
         <p>Teste</p>
         <GlobalStyle />
       </>
-    )
+    );
   }
 }
